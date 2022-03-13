75fc492d279cf7875c7ad64e0c9d5eb3e3a52823
diamant711 <53996598+diamant711@users.noreply.github.com>
2022-03-13 12:21:15 +0100

    Connection & Server class continued

--- src/Connection.hpp
+++ src/Connection.hpp
@@ -1,19 +1,28 @@
-#include <boost/asio.hpp>
+///////////////////////////////////////////////////////////////////////////////
+//
+//    Connection.hpp specifies a class that represent a connection and define
+//    the wrapper function for input-output socket operation using an internal
+//    buffer
+//
+///////////////////////////////////////////////////////////////////////////////
 
-class Connection {
+#include <boost/asio.hpp>
+#include <memory>
+
+class Connection : public std::enable_shared_from_this<Connection> {
   public:
     Connection();
     ~Connection();
-    bool write(void);
-    bool read(void);
+    bool send(void);
+    bool recive(void);
     void load_data(const std::string &);
     void load_data(const std::vector<int> &);
     void load_data(const std::vector<unsigned char> &);
+    void load_data(const std::shared_ptr<char>);
     void load_data(const boost::asio::mutable_buffer &);
-    const boost::asio::mutable_buffer& unload_data(void) const;
+    const std::shared_ptr<char> unload_data(void) const;
     const boost::asio::ip::tcp::socket& get_socket() const;
   private:
     boost::asio::ip::tcp::socket m_socket;
     boost::asio::mutable_buffer m_internal_buffer;
-    void m_handle_write(boost::system::error_code&, size_t);
 };
--- src/Server.hpp
+++ src/Server.hpp
@@ -1,20 +1,26 @@
-///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //
-//  Serber.hpp specifies a class which define
-//  the base virtual methods that define a server
+//  Serber.hpp specifies a class which define the base virtual methods that 
+//  define an asyncronus server
 //
-/////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 #include <boost/asio.hpp>
+#include "Connection.hpp"
 
 class Server {
   public:
     Server();
     ~Server();
     virtual void accept(void);
-    virtual void reply(const std::function<std::string& (void)> &);
-    const std::string& get_remote_ip() const;
-  private:
+    virtual void close(void); /*Garbage collector bad*/
+  protected:
+    //Variables
+    typedef struct {
+      shared_ptr<Connection> connection_ptr;
+      int status_id;
+      bool end_of_life;
+      boost::asio::address remote_ip;
+    } m_connection_database_record_t;
+    std::map<int, m_connection_database_record_t> m_connection_database;
     boost::asio::io_context m_io_context;
-    const boost::asio::ip::address m_remote_address;
-    bool m_connected = false;
 };
