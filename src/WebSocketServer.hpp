204832567be84d443e117cba6fe3cfac407f6466
riccardoosvaldo nana <riccardoosvaldo.nana@lab06.labcalcolo.lab>
2022-05-17 12:16:53 +0200

    Continued WebServerSocket NOT_READY

--- src/WebSocketServer.hpp
+++ src/WebSocketServer.hpp
@@ -11,31 +11,49 @@
 
 #include <memory>
 
+#include "TCPServer"
 #include "Connection.hpp"
 
-class WebSocketServer {
+class WebSocketServer : TCPServer {
   // Functions
   public:
     WebSocketServer(boost::asio::io_context &);
     ~WebSocketServer(void);
 
-    void accept(void);
+    void respond(void);
   private:
-  
+    bool m_handshake_builder(void);
+
   // Variables
+  typedef enum {
+    HANDSHAKE_ANSWARE,
+    UPDATING_CLIENT,
+    CLOSING_CONNECTION
+  } m_status_t;
+  m_status_t status;
   unique_ptr<boost::asio::io_context> m_io_context_ptr;
   unique_ptr<Connection> m_connection_ptr;
 
 };
 
-WebSocketServer::WebSocketServer(boost::asio::io_context& tmp) {
-  m_io_context_ptr.reset(&tmp);
+WebSocketServer::WebSocketServer(boost::asio::io_context& executor) {
+  m_io_context_ptr.reset(&executor);
 }
 
 WebSocketServer::~WebSocketServer(void) {}
 
-void WebSocketServer::accept(void) {
-  
+void WebSocketServer::respond(void) {
+  m_get_executor().poll();
+  if(!m_is_waiting_list_empty()) {
+    switch (status) {
+      case HANDSHAKE_ANSWARE:
+      break;
+      case UPDATING_CLIENT:
+      break;
+      case CLOSING_CONNECTION:
+      break;
+    }
+  }
 }
 
 #endif
