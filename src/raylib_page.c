7e3d71fa221b4b16b68bca7b85ff9586ba642e83
Eleod99 <96332563+Eleod99@users.noreply.github.com>
2022-06-11 16:00:04 +0200

    local comments for raylib_page.c.Completed

--- src/raylib_page.c
+++ src/raylib_page.c
@@ -3,7 +3,6 @@
 *   raylib [core] example - Basic window (adapted for HTML5 platform)
 *
 *   This example is prepared to compile for PLATFORM_WEB, PLATFORM_DESKTOP and PLATFORM_RPI
-*   As you will notice, code structure is slightly diferent to the other examples...
 *   To compile it for PLATFORM_WEB just uncomment #define PLATFORM_WEB at beginning
 *
 *   This example has been created using raylib 1.3 (www.raylib.com)
@@ -14,11 +13,11 @@
 ********************************************************************************************/
 
 #include <stdlib.h>
-
+//Colour palette
 #include "../inc/jet.h"
 #include "../inc/raylib.h"
 
-#include "../inc/temperature_matrix.h"
+#include "../inc/temperature_matrix.h" 
 
 #define PIXEL 4
 #define WINDOW_WIDTH ((PIXEL * NX) + 3 * NX)
@@ -39,22 +38,17 @@ typedef enum
 #include <emscripten/emscripten.h>
 #endif
 
-//------------------------------------------------------------------------------
-// Global Variables Definition
-//------------------------------------------------------------------------------
-//const int screenWidth = 800;
-//const int screenHeight = 450;
-//------------------------------------------------------------------------------
-// Module Functions Declaration
-//------------------------------------------------------------------------------
-
+// This function takes a temperature as input and returns its respective
+// colour as defined by the jet.h palette.
 Color TtoC (double, double, double);
 
-void UpdateDrawFrame (void); // Update and Draw one frame
+// This function updates and draws a frame.
+void UpdateDrawFrame (void);
 
 //------------------------------------------------------------------------------
 // Main Enry Point
 //------------------------------------------------------------------------------
+
 int
 main ()
 {
@@ -90,14 +84,13 @@ main ()
 Color
 TtoC (double cold, double hot, double t)
 {
-  /*
-  (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-  */
   if ((t <= hot) && (t >= cold))
     {
       int index = (t - cold) * (JETSIZE - 1) / (hot - cold);
       return jet[index];
     }
+  // This else detects if the input temperature does not fit in the (cold, hot)
+  // range, and sets the correspective pixel to black.
   else
     {
       return (Color){ 0, 0, 0, 255 };
@@ -107,9 +100,8 @@ TtoC (double cold, double hot, double t)
 void
 UpdateDrawFrame (void)
 {
-  // Update
+  // Updates the graphic simulation according to the user's input.
   //----------------------------------------------------------------------------
-  // TODO: Update your variables here
   static int n_step = 0;
   static player_status_t player_status = STOP;
   static player_status_t old_player_status = STOP;
